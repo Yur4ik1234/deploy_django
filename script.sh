e680f2ccd51c1f89be04447b6e6cc1a8aafe9281
Yur4ik1234 <60612565+Yur4ik1234@users.noreply.github.com>
2022-07-17 15:38:55 +0300

    changes to script

--- script.sh
+++ script.sh
@@ -3,4 +3,4 @@ terraform apply -auto-approve
 az vmss list-instance-public-ips --name example-vmss  --resource-group azure-resource_group > file.txt
 cat file.txt | grep 'ipAddress' | awk  '{print $2}' | tr -d , | sed 's/^.//;s/.$//' > ip.txt
 python3 iphost.py >> host.txt
-ansible-playbook -i host.txt  ansible/prot.yaml
+ansible-playbook -i host.txt  prot.yaml
